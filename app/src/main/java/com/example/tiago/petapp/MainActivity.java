6edcc32b92e39260ef1021db7dc0d877236a8b83
Tiago Pereira <tiagofpp4@gmail.com>
2017-12-27 02:57:10 +0000

    Login e Register design 2

--- app/src/main/java/com/example/tiago/petapp/MainActivity.java
+++ app/src/main/java/com/example/tiago/petapp/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setBackgroundDrawableResource(R.drawable.background);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
 
--- app/src/main/java/com/example/tiago/petapp/RegistarActivity.java
+++ app/src/main/java/com/example/tiago/petapp/RegistarActivity.java
@@ -2,6 +2,7 @@ package com.example.tiago.petapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class RegistarActivity extends Activity {
 
@@ -9,5 +10,7 @@ public class RegistarActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+        getWindow().setBackgroundDrawableResource(R.drawable.background);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
