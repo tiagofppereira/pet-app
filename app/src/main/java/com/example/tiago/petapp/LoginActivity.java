a6c60c48e5fbfdcb9430632503cf6fff85392dce
Tiago Pereira <tiagofpp4@gmail.com>
2017-12-28 16:48:05 +0000

    Menu

---
+++ app/src/main/java/com/example/tiago/petapp/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.tiago.petapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getWindow().setBackgroundDrawableResource(R.drawable.background);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+}
+
